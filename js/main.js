48016d2df18888e966573b29e8cb015b3651b112
Maddie Fees <madison.fees90@gmail.com>
2015-01-31 13:34:21 -0700

    @mfees @carolinapowers, moving button html in js file and making them display on bottom

--- js/main.js
+++ js/main.js
@@ -105,6 +105,11 @@ var board = {};
 
 print(board);
 
+document.write('<i class="fa fa-angle-double-left fa-lg"></i>');
+document.write('<i class="fa fa-angle-left fa-lg"></i>');
+document.write('<i class="fa fa-angle-right fa-lg"></i>');
+document.write('<i class="fa fa-angle-double-right fa-lg"></i>');
+
 
 //var board = [
 //    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
