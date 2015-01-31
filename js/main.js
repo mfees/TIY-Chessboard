c6a534d68706f854c0d6943a27656645affcdfb5
Maddie Fees <madison.fees90@gmail.com>
2015-01-31 11:37:23 -0700

    printing the board and the pieces

--- js/main.js
+++ js/main.js
@@ -5,13 +5,11 @@
 function print(board){
     document.write('<table>');
     
-    var cell = "<td></td>";
-    var row = "<tr></tr>";
-    
-    _.times(8, function(){
+    _.forEach(board, function(row, key){
         document.write("<tr>");
-        _.times(8, function(){
-            document.write(cell);
+        _.forEach(row, function(square, k){
+            document.write("<td>" + square + "</td>")
+   
         });
         document.write("</tr>");
     });
@@ -20,7 +18,92 @@ function print(board){
     
 }
 
-print();
+var board = {};
+    board.a = {
+        'one': '<span class="">&#9820;</span>',
+        'two': '<span class="">&#9822;</span>',
+        'three': '<span class="">&#9821;</span>',
+        'four': '<span class="">&#9819;</span>',
+        'five': '<span class="">&#9818;</span>',
+        'six': '<span class="">&#9821;</span>',
+        'seven': '<span class="">&#9822;</span>',
+        'eight': '<span class="">&#9820;</span>'
+    };
+    board.b = {
+        'one': '&#9823;',
+        'two': '&#9823;',
+        'three': '&#9823;',
+        'four': '&#9823;',
+        'five': '&#9823;',
+        'six': '&#9823;',
+        'seven': '&#9823;',
+        'eight': '&#9823;'
+    };
+    board.c = {
+        'one': '',
+        'two': '',
+        'three': '',
+        'four': '',
+        'five': '',
+        'six': '',
+        'seven': '',
+        'eight': ''
+    };
+    board.d = {
+        'one': '',
+        'two': '',
+        'three': '',
+        'four': '',
+        'five': '',
+        'six': '',
+        'seven': '',
+        'eight': ''
+    };
+    board.e = {
+        'one': '',
+        'two': '',
+        'three': '',
+        'four': '',
+        'five': '',
+        'six': '',
+        'seven': '',
+        'eight': ''
+    };
+    board.f = {
+        'one': '',
+        'two': '',
+        'three': '',
+        'four': '',
+        'five': '',
+        'six': '',
+        'seven': '',
+        'eight': ''
+    };
+    board.g = {
+        'one': '&#9823;',
+        'two': '&#9823;',
+        'three': '&#9823;',
+        'four': '&#9823;',
+        'five': '&#9823;',
+        'six': '&#9823;',
+        'seven': '&#9823;',
+        'eight': '&#9823;'
+    };
+    board.h = {
+        'one': '<span class="">&#9820;</span>',
+        'two': '<span class="">&#9822;</span>',
+        'three': '<span class="">&#9821;</span>',
+        'four': '<span class="">&#9819;</span>',
+        'five': '<span class="">&#9818;</span>',
+        'six': '<span class="">&#9821;</span>',
+        'seven': '<span class="">&#9822;</span>',
+        'eight': '<span class="">&#9820;</span>'
+    };
+
+//board.c.three = board.a.one;
+//board.a.one = '';
+
+print(board);
 
 
 //var board = [
